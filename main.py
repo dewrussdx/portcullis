0a31da8e643dc8541108e3aa1f4d7c123eff449d
Dale Russell <russdx@dewconsultingllc.com>
2023-07-08 15:47:14 -0400

    Robustness and error handling pass. Add prelim swing trading support. Cleanup APIs

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from portcullis.portfolio import Portfolio, Asset
+from portcullis.portfolio import Portfolio
 
 
 def investment_portfolio_sample():
-    portfolio = Portfolio([
-        'GOOG', 'SBUX', 'KSS', 'NEM'
-    ])
-
-    weights = portfolio.optimize(risk_free_rate=0.03/252,
-                                 start='2014-01-01', end='2014-07-01')
-    if weights is not None:
-        print(f'Sharp Ratio: {portfolio.get_sharp_ratio()}')
-        print(f'Weights: {portfolio.get_weights()}')
+    start = '2018-07-01'
+    interval = '1d'
+    # top10_sp500 = Portfolio.filter_top_return_symbols(
+    #    Portfolio.fetch_sp500_symbols(), 10, start=start, interval=interval)
+    top10_sp500 = ['ENPH', 'TSLA', 'MRNA', 'GEHC',
+                   'CEG', 'AMD', 'SEDG', 'NVDA', 'CARR', 'DXCM']
+    print(top10_sp500)
+    sp500 = Portfolio(top10_sp500)
+    if sp500.optimize(risk_free_rate=0.03/252, start=start, interval=interval):
+        print(f'Sharp Ratio: {sp500.get_sharp_ratio()}')
+        print(f'Weights: {sp500.get_weights()}')
 
 
 def swingtrading_portfolio_sample():
@@ -20,28 +22,15 @@ def swingtrading_portfolio_sample():
 
     asset = portfolio['META'].asset
     min, max = asset.get_swing_bounds(
-        start='2018-07-01', interval='1wk')
+        start='2018-07-01', interval='1wk') or (0, 0)
     print(asset)
     print(' Min: %.2f %%' % (min * 100.0))
     print('Mean: %.2f %%' % (max * 100.0))
 
-    asset = portfolio['SMH'].asset
-    min, max = asset.get_swing_bounds(
-        start='2018-07-01', interval='1wk')
-    print(asset)
-    print(' Min: %.2f %%' % (min * 100.0))
-    print('Mean: %.2f %%' % (max * 100.0))
- 
-    asset = portfolio['XBI'].asset
-    min, max = asset.get_swing_bounds(
-        start='2018-07-01', interval='1wk')
-    print(asset)
-    print(' Min: %.2f %%' % (min * 100.0))
-    print('Mean: %.2f %%' % (max * 100.0))
 
 def main():
     investment_portfolio_sample()
-    swingtrading_portfolio_sample()
+    # swingtrading_portfolio_sample()
 
 
 if __name__ == "__main__":
--- portcullis/portfolio.py
+++ portcullis/portfolio.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import yfinance.shared as yfs
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize
@@ -7,6 +8,9 @@ from scipy.optimize import minimize
 Asset class
 Describes an asset traded on financial exchanges
 '''
+
+sp500_index = ['CDW', 'GM', 'GRMN', 'STE', 'STX', 'CPRT', 'CBRE', 'CMI', 'WFC', 'MA', 'BIO', 'MDT', 'DGX', 'AKAM', 'IPG', 'EBAY', 'LMT', 'ACGL', 'ACN', 'BRO', 'CSCO', 'VTRS', 'AJG', 'ORLY', 'STT', 'AZO', 'MS', 'COF', 'ROST', 'CHTR', 'VRSK', 'WM', 'OKE', 'MDLZ', 'BXP', 'ADP', 'PTC', 'HBAN', 'CCI', 'PSA', 'PPG', 'PEAK', 'TT', 'PODD', 'MPWR', 'KIM', 'HST', 'BKNG', 'FANG', 'TAP', 'MOS', 'JBHT', 'AMAT', 'RMD', 'XEL', 'GS', 'SWKS', 'AWK', 'SBUX', 'MOH', 'FAST', 'SHW', 'BBWI', 'DPZ', 'DIS', 'FTNT', 'FMC', 'HON', 'ALLE', 'GL', 'HLT', 'PFG', 'PANW', 'EOG', 'PGR', 'WELL', 'PNR', 'WYNN', 'K', 'LVS', 'JCI', 'FDS', 'DHR', 'CAH', 'NTAP', 'ALB', 'NSC', 'FLT', 'APA', 'CI', 'AVGO', 'EW', 'HSIC', 'MO', 'WAT', 'AON', 'CPB', 'HSY', 'ITW', 'PLD', 'HAL', 'MCK', 'LIN', 'KEY', 'NCLH', 'QCOM', 'IT', 'PH', 'ETN', 'RSG', 'XOM', 'GOOGL', 'NEE', 'MMC', 'NFLX', 'AAL', 'ATVI', 'TMO', 'UPS', 'ADBE', 'ARE', 'NOC', 'EL', 'LKQ', 'FIS', 'CHD', 'FI', 'BLK', 'NRG', 'PRU', 'IEX', 'V', 'ATO', 'AXON', 'CPT', 'ODFL', 'IDXX', 'BG', 'ZION', 'BK', 'URI', 'ANET', 'NEM', 'RJF', 'AXP', 'WTW', 'TRMB', 'ADI', 'FICO', 'JNJ', 'TGT', 'BKR', 'DFS', 'ROK', 'COST', 'SO', 'RTX', 'ON', 'AMCR', 'BIIB', 'TXT', 'DUK', 'CME', 'TSCO', 'IFF', 'SYY', 'IQV', 'TEL', 'WHR', 'EQIX', 'ABC', 'F', 'META', 'PHM', 'KDP', 'SBAC', 'XRAY', 'VRTX', 'CINF', 'A', 'STZ', 'LEN', 'IVZ', 'TXN', 'STLD', 'GD', 'GPN', 'TDG', 'AMD', 'GPC', 'AMP', 'WRB', 'ENPH', 'BSX', 'ZTS', 'FSLR', 'KLAC', 'INCY', 'AOS', 'DLTR', 'ABT', 'MAA', 'MTB', 'TRV', 'YUM', 'PXD', 'WAB', 'POOL', 'MTCH', 'TPR', 'GILD', 'TSLA', 'L', 'DG', 'CRM', 'SYK', 'TECH', 'IP', 'SPG', 'VLO', 'RL', 'PFE', 'SNA', 'ABBV', 'FRT', 'LH', 'HES', 'UNH', 'INTU', 'BWA', 'PPL', 'CF', 'DVA', 'DD', 'EIX',
+               'ICE', 'BAX', 'LNT', 'GE', 'HD', 'MSI', 'UNP', 'DVN', 'LYV', 'FITB', 'CMCSA', 'EA', 'FCX', 'CE', 'PCG', 'O', 'WEC', 'HUM', 'MPC', 'ISRG', 'MGM', 'APTV', 'WMT', 'CSGP', 'MRO', 'EMN', 'MKTX', 'CMG', 'ROP', 'PWR', 'JPM', 'EXPE', 'UAL', 'HRL', 'RHI', 'PAYC', 'EQT', 'NTRS', 'TYL', 'BMY', 'CL', 'TFX', 'AAP', 'LOW', 'PM', 'PAYX', 'MHK', 'PEP', 'VTR', 'EFX', 'BBY', 'CSX', 'EVRG', 'NWL', 'HCA', 'ROL', 'ED', 'AEE', 'KMB', 'UHS', 'KMI', 'PG', 'LRCX', 'ULTA', 'CVS', 'CAG', 'SEE', 'AVB', 'CMA', 'TDY', 'ESS', 'BEN', 'VMC', 'WBD', 'BR', 'ILMN', 'KMX', 'EXPD', 'HPQ', 'AME', 'HOLX', 'DHI', 'CAT', 'BAC', 'RVTY', 'ES', 'SRE', 'MRK', 'APH', 'ADSK', 'NVDA', 'MU', 'C', 'CLX', 'NOW', 'TROW', 'CB', 'DTE', 'GNRC', 'IBM', 'WST', 'APD', 'GEN', 'GOOG', 'NUE', 'LUV', 'PKG', 'MLM', 'RCL', 'TJX', 'CCL', 'DOV', 'GLW', 'SNPS', 'WY', 'KR', 'XYL', 'MNST', 'PARA', 'INTC', 'EXC', 'VFC', 'PNW', 'TTWO', 'NDSN', 'OMC', 'AVY', 'SPGI', 'NKE', 'GWW', 'CTSH', 'OXY', 'TMUS', 'CRL', 'DLR', 'LLY', 'JNPR', 'ELV', 'RF', 'MET', 'ECL', 'CNC', 'EMR', 'DRI', 'CMS', 'PSX', 'T', 'UDR', 'COO', 'CBOE', 'ZBH', 'CHRW', 'SWK', 'PNC', 'FFIV', 'MCD', 'PCAR', 'MMM', 'WBA', 'TSN', 'AMGN', 'NDAQ', 'BALL', 'SLB', 'HIG', 'BDX', 'ORCL', 'REGN', 'DXC', 'AMT', 'AFL', 'EXR', 'MAS', 'USB', 'ETR', 'EQR', 'DAL', 'HAS', 'AEP', 'NVR', 'WDC', 'FDX', 'GIS', 'TFC', 'ANSS', 'BA', 'REG', 'SJM', 'NWS', 'NI', 'LYB', 'IRM', 'DXCM', 'PEG', 'VRSN', 'CNP', 'CTAS', 'TRGP', 'NXPI', 'SCHW', 'LNC', 'EPAM', 'CTRA', 'DE', 'MTD', 'MCHP', 'WMB', 'D', 'AIZ', 'ALK', 'VZ', 'ALGN', 'RE', 'MSFT', 'AES', 'TER', 'AMZN', 'MSCI', 'HII', 'AAPL', 'CDNS', 'COP', 'ALL', 'AIG', 'NWSA', 'ZBRA', 'FE', 'ADM', 'JKHY', 'MCO', 'LHX', 'MKC', 'KO', 'LDOS', 'J', 'MAR', 'CVX']
 
 
 class Asset:
@@ -33,28 +37,38 @@ class Asset:
 
     # Fetch timeseries for this symbol
     def get_timeseries(self, **kwargs) -> pd.DataFrame:
-        return pd.DataFrame(yf.download(
-            self.symbol, **kwargs))
+        print(f'Downloading timeseries for {self.symbol}...')
+        data = yf.download(self.symbol, **kwargs)
+        return pd.DataFrame(data) if not yfs._ERRORS.keys() else None
 
     # Fetch timeseries and compute returns
     def get_returns(self, **kwargs) -> pd.DataFrame:
         series = self.get_timeseries(**kwargs)
-        data = series['Close'].pct_change(
-            fill_method='ffill').rename(self.symbol)
-        return pd.DataFrame(index=series.index[1:], data=data[1:] * 100)
+        if series is not None:
+            close = series['Close']
+            assert close.isna().sum().sum() == 0 # Sanity check, should not happen here
+            returns = close.pct_change(
+                fill_method='ffill').rename(self.symbol)
+            # TODO: Figure out why some notation have pct_change multiplied by 100, we omit here: data=data[1:]*100
+            return pd.DataFrame(index=series.index[1:], data=returns[1:])
+        else:
+            return None
 
     # Compute swing bounds (minimum range, mean range)
     def get_swing_bounds(self, **kwargs):
         series = self.get_timeseries(**kwargs)
-        high = Asset.fill_na(series['High'])
-        low = Asset.fill_na(series['Low'])
-        column = high / low - 1.0
-        return column.min(), column.mean()
+        if series is not None:
+            high = Asset.fill_na(series['High'])
+            low = Asset.fill_na(series['Low'])
+            column = high / low - 1.0
+            return column.min(), column.mean()
+        else:
+            return None, None
 
 
 '''
 Portfolio class
-Describes a portfolio of finanical assets
+Describes a portfolio of financial assets
 '''
 
 
@@ -67,6 +81,7 @@ class Portfolio:
         def __str__(self):
             return f'{self.asset} x {self.weight}'
 
+    # Constructor
     def __init__(self, symbols: list[str] = None):
         self._values = dict()
         self.sharp_ratio = None
@@ -79,7 +94,28 @@ class Portfolio:
         std = np.sqrt(weights.dot(cov).dot(weights))
         return (expected_return-risk_free_rate)/std
 
-    # Add list of assets to the portfolio
+    # Fetch list of S&P 500 from wikipedia
+    @staticmethod
+    def fetch_sp500_symbols() -> list[str]:
+        df = pd.read_html(
+            'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+        return df['Symbol'].tolist()
+
+    # Compute top N symbols based on returns
+    @staticmethod
+    def filter_top_return_symbols(symbols, N, **kwargs) -> list[str]:
+        pool = []
+        for symbol in symbols:
+            series = Asset(symbol).get_returns(**kwargs)
+            if series is not None:
+                pool.append((symbol, series[symbol].mean()))
+        pool.sort(key=lambda x: x[1], reverse=True)
+        top = []
+        for i in range(min(N, len(pool))):
+            top.append(pool[i][0])
+        return top
+
+    # Add multiple assets to the portfolio
     def add_symbols(self, symbols: list[str]) -> None:
         # Convert list of symbols to portfolio items
         for symbol in symbols:
@@ -93,29 +129,42 @@ class Portfolio:
         # Insert item into portfolio
         self._values[symbol] = Portfolio.Item(symbol)
 
-    # Remove asset
+    # Remove an asset from the portfolio
+    # Note: This requires any weight lists to be refreshed, e.g. using "Portfolio.get_weights()"
     def remove_symbol(self, symbol: str) -> None:
         self._values.pop(symbol.upper(), None)
 
     # Return dataframe with returns of the portfolio
     def get_returns(self, **kwargs) -> pd.DataFrame:
+        invalids = []
         returns = None
-        for _, value in self._values.items():
+        for key, value in self._values.items():
             df = value.asset.get_returns(**kwargs)
-            if returns is None:
-                returns = pd.DataFrame(index=df.index, data=df)
+            if df is None:
+                invalids.append(key)
             else:
-                returns = returns.join(df)
+                if returns is None:
+                    returns = pd.DataFrame(index=df.index, data=df)
+                else:
+                    returns = returns.join(df)
+        if len(invalids) > 0:
+            print(f'Removing invalid symbols {invalids} from portfolio...')
+            for invalid in invalids:
+                self.remove_symbol(invalid)
         return returns
 
     # Compute optimal portfolio (maximize sharp ratio) and return the weights
     def optimize(self, bounds=None, risk_free_rate=0.0, **kwargs) -> bool:
-        D = len(self._values)
         # Set default bounds (allowing short sales) if not specified
         bounds = bounds or (-0.5, None)
         print(f'Bounds: {bounds}')
         print(f'Risk Free Rate: {risk_free_rate}')
         returns = self.get_returns(**kwargs)
+        if returns is None:
+            print(f'No asset in portfolio, nothing to optimize - bailing out...')
+            return False
+        D = len(self._values)
+        assert D > 0 and len(returns.columns) == D
         mean_return = returns.mean()
         cov = returns.cov()
 
@@ -126,8 +175,12 @@ class Portfolio:
         # Objective: Minimize the negative Sharp ratio to find the optimal portfolio
         def objective_minimize_neg_sharp_ratio(weights):
             expected_return = weights.dot(mean_return)
+            # Variance
+            var = weights.dot(cov).dot(weights)
+            assert var >= 0
             # Risk (or Standard deviation) == sqrt(variance)
-            std = np.sqrt(weights.dot(cov).dot(weights))
+            std = np.sqrt(var)
+            assert std != 0
             # Negate sharp ratio (to turn mimization into maximization)
             return -(expected_return-risk_free_rate)/std
 
@@ -142,13 +195,17 @@ class Portfolio:
                 }
             ],
             bounds=[bounds] * D,
+            options={
+                'maxiter': 200,
+            }
         )
-        if result.status == 0:
-            self.sharp_ratio = -result.fun
-            self._apply_weights(result.x)
-            # Return sharp_ratio and portfolio weights
-            return True
-        return False
+
+        if result.status != 0:
+            print(f'*** ERROR: Optimizer failed => {result}')
+            return False
+        self.sharp_ratio = -result.fun
+        self._apply_weights(result.x)
+        return True
 
     # Return the Sharp retio of this portfolio
     def get_sharp_ratio(self) -> float:
@@ -167,8 +224,9 @@ class Portfolio:
         for _, value in self._values.items():
             asset = value.asset
             series = asset.get_timeseries(**kwargs)
-            series['Name'] = asset.symbol
-            frames.append(series)
+            if series:
+                series['Name'] = asset.symbol
+                frames.append(series)
         pd.concat(frames).to_csv(csv)
 
     # Apply weights to items
